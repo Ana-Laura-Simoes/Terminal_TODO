fe885ad96764d1ffb129c9b0fd14b10d9a68eca6
Ana Simoes <ana.simoes-mato@unesp.br>
2021-06-07 19:28:27 -0300

    ajustes e loop

--- programa.js
+++ programa.js
@@ -1,38 +1,52 @@
-var list=["codar","repirar","dormir"];
 
-var readlineSync = require('readline-sync'),
-options = ['add', 'list','check', 'remove'],
+var list=[];
+var cancel=true;
+
+while(cancel){
+const chalk = require('chalk');
+var readlineSync = require('readline-sync');
+var options = ['add', 'list','check', 'remove'],
 index = readlineSync.keyInSelect(options, 'Type your command:');
+console.log(options[index]);
+
+switch (options[index]) {
+    case 'add':
+        var ToDo = readlineSync.question("What do you want to do? ");
+        list.push({task:ToDo,status:false});
+    break;
+
+    case 'list':
+        for(let i=0;i<list.length;i++){
+            if(list[i].status===false) console.log(chalk.red(list[i].task));
+            else console.log(chalk.green(list[i].task));
+            ;
+        }
+    break;
+      
+    case 'check':
+        var newLIST=list.map((task)=>task.status?chalk.green(task.task):chalk.red(task.task));
+        
+        var ind = readlineSync.keyInSelect(newLIST, 'What todo do you want check/uncheck:');
+   
+        var newlist=list.map((task,i)=> {if(i===ind)task.status=!task.status})   ; 
+    break;
+    
+    case 'remove':
+        var newLIST=list.map((task)=>task.status?chalk.green(task.task):chalk.red(task.task));
+        var ind = readlineSync.keyInSelect(newLIST, 'What todo do you want remove:');
+
+        var newlist = list.filter((i)=>i!==list[ind]);
+        list=newlist;
+        
+    break;     
 
 
-if(options[index]==="add") {
-    var ToDo = readlineSync.question("What do you want to do? ");
-    //list = list + ToDo;
-    list.push(ToDo);
-
-};
-
-if(options[index]==="list"){
-
-    for(let i=0;i<list.length;i++){
-        console.log(list[i]);
+    case undefined:
+        cancel=false;
+    break;   
     }
 }
 
-if(options[index]==="check"){
-        
-     var ind = readlineSync.keyInSelect(list, 'What todo do you want check/uncheck:');
-
-}
-
-if(options[index]==="remove"){
-    var ind = readlineSync.keyInSelect(list, 'What todo do you want remove:');
-
-var newli = list.filter((i)=>i!==list[ind]);
-list=newli;
-
-    console.log(list);
-}
 
 
 
